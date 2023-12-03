553cd3ede5b7474447ff671f584b6e5829ef2f42
Damir <78422997+DamirMrtv@users.noreply.github.com>
2023-12-03 17:36:03 +0600

    17 chapter

--- Project/cmd/api/routes.go
+++ Project/cmd/api/routes.go
@@ -7,19 +7,17 @@ import (
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
-	// Convert the notFoundResponse() helper to a http.Handler using the
-	// http.HandlerFunc() adapter, and then set it as the custom error handler for 404
-	// Not Found responses.
+
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
-	// Likewise, convert the methodNotAllowedResponse() helper to a http.Handler and set
-	// it as the custom error handler for 405 Method Not Allowed responses.
+
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/edtoys", app.listEdToysHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/edtoys", app.createEdtoysHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/edtoys/:id", app.showEdtoysHandler)
-	router.HandlerFunc(http.MethodPatch, "/v1/edtoys/:id", app.updateEdToysHandler)
-	router.HandlerFunc(http.MethodDelete, "/v1/edtoys/:id", app.deleteEdToysHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/edtoys", app.requirePermission("edtoys:read", app.listEdToysHandler))
+	router.HandlerFunc(http.MethodPost, "/v1/edtoys", app.requirePermission("edtoys:write", app.createEdtoysHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/edtoys/:id", app.requirePermission("edtoys:read", app.showEdtoysHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/edtoys/:id", app.requirePermission("edtoys:write", app.updateEdToysHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/edtoys/:id", app.requirePermission("edtoys:write", app.deleteEdToysHandler))
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
